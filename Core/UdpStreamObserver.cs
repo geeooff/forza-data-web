25f30ac28dda288fa6e60e8559425fb38fb15813
Geoffrey Vancoetsem <geoff@geoffzone.net>
2019-04-25 20:01:18 +0200

    Raw UDP Stream Observable/Observer implementation

---
+++ Core/UdpStreamObserver.cs
@@ -0,0 +1,37 @@
+﻿using ForzaData.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForzaData.Core
+{
+	public abstract class UdpStreamObserver : IObserver<byte[]>
+	{
+		private IDisposable _unsubscriber;
+
+		public UdpStreamObserver()
+		{
+			
+		}
+
+		public void Subscribe(IObservable<byte[]> listener)
+		{
+			_unsubscriber = listener.Subscribe(this);
+		}
+
+		public void Unsubscribe()
+		{
+			if (_unsubscriber != null)
+			{
+				_unsubscriber.Dispose();
+				_unsubscriber = null;
+			}
+		}
+
+		public abstract void OnCompleted();
+
+		public abstract void OnError(Exception error);
+
+		public abstract void OnNext(byte[] value);
+	}
+}
---
+++ Core/UdpStreamUnsubscriber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForzaData.Core
+{
+	internal class UdpStreamUnsubscriber : IDisposable
+	{
+		private readonly ICollection<IObserver<byte[]>> _observers;
+		private readonly IObserver<byte[]> _observer;
+
+		internal UdpStreamUnsubscriber(ICollection<IObserver<byte[]>> observers, IObserver<byte[]> observer)
+		{
+			_observers = observers;
+			_observer = observer;
+		}
+
+		public void Dispose()
+		{
+			if (_observers.Contains(_observer))
+			{
+				_observers.Remove(_observer);
+			}
+		}
+	}
+}
